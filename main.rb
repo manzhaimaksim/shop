0e4ee47293f2e150ee7a4931165e5f35eb8c50bb
manzhaimaksim <maxbbb88@gmail.com>
2020-04-01 20:26:38 +0300

    Edit main program

--- main.rb
+++ main.rb
@@ -2,5 +2,26 @@ require_relative 'lib/product'
 require_relative 'lib/book'
 require_relative 'lib/movie'
 
-movie = Movie.new(price: 290, amount: 4)
+products = []
 
+products << Movie.new(price: 100,
+                      amount: 10,
+                      title: 'Леон',
+                      director: 'Люк Бессон',
+                      year: 1994)
+
+products << Movie.new(price: 150,
+                      amount: 5,
+                      title: 'Дурак',
+                      director: 'Юрий быков',
+                      year: 2014)
+
+products << Book.new(price: 400,
+                      amount: 25,
+                      title: 'Мастер и Маргарита',
+                      author: 'Михаил Булгаков',
+                      genre: 'роман')
+
+puts 'Вот какие товары у нас есть'
+puts
+products.each { |product| puts product }
