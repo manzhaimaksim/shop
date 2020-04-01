717032d62d2bdfa686c177a2f18aa59d775f0572
manzhaimaksim <maxbbb88@gmail.com>
2020-04-01 20:26:18 +0300

    Edit classes

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,2 +1,14 @@
 class Movie < Product
+  attr_reader :director, :year
+
+  def initialize(params)
+    super
+
+    @director = params[:director]
+    @year = params[:year]
+  end
+
+  def to_s
+    "Фильм #{@title}, #{@year}, Режиссер - #{@director}, #{super}"
+  end
 end
--- lib/product.rb
+++ lib/product.rb
@@ -1,8 +1,13 @@
 class Product
-  attr_reader :price, :amount
+  attr_reader :price, :amount, :title
 
-  def initialize(price:, amount:)
-    @price = price
-    @amount = amount
+  def initialize(params)
+    @price = params[:price]
+    @amount = params[:amount]
+    @title = params[:title]
+  end
+
+  def to_s
+    "#{@price} руб. (осталось #{@amount})"
   end
 end
